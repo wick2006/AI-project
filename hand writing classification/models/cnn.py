671444a6019bba3077a5e92c6011806556957026
Wick Ko <1838413566@qq.com>
2025-12-22 16:19:50 +0800

    Add files via upload

--- hand writing classification/models/cnn.py
+++ hand writing classification/models/cnn.py
@@ -5,19 +5,19 @@ class Net(nn.Module):
     def __init__(self, c):
         super(Net, self).__init__()
         self.conv1 = nn.Sequential( # conv卷积层，ReLU为激活函数，MaxPool2d为池化层
-            nn.Conv2d(3, 32, 3, 1, 1),  # 32x28x28
+            nn.Conv2d(3, 32, 3, 1, 1),  # 输入通道rgb3通道，输出通道32个特征图，卷积核3x3，步长1，填充1
             nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(2)#2x2的池化
         )  # 32x14x14
         self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 64, 3, 1, 1),  # 64x14x14
+            nn.Conv2d(32, 64, 3, 1, 1),  #32通道到64通道，3x3卷积核，步长1，填充1
             nn.ReLU(),
-            nn.MaxPool2d(2)  # 64x7x7
+            nn.MaxPool2d(2)  #减半输出 64x7x7
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(64, 64, 3, 1, 1),  # 64x7x7
+            nn.Conv2d(64, 64, 3, 1, 1),  # 输入64x7x7
             nn.ReLU(),
-            nn.MaxPool2d(2)  # 64x3x3
+            nn.MaxPool2d(2)  # 输出64x3x3
         )
         self.dense = nn.Sequential(
             nn.Linear(64 * 3 * 3, 128),  # fc4 64*3*3 -> 128
